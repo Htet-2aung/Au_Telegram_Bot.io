456d9491b031993c3a2396e5bc77219207c47998
Htet Aung <kienbrown212001@gmail.com>
2024-09-27 20:32:55 +0700

    Delete js directory

--- js/charge.js
+++
@@ -1,8 +0,0 @@
-setInterval(()=> {
-    let count = localStorage.getItem('count')
-    let total = localStorage.getItem('total')
-    let power = localStorage.getItem('power');
-    if(Number(total) > power){
-        localStorage.setItem('power' , `${Number(power) + Number(count)}`);
-    }
-}, 1000);
